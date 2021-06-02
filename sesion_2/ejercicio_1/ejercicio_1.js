79086cb51f08a31cdccf6890f28fea2248411737
angelechavarriag <82201584+angelechavarriag@users.noreply.github.com>
2021-06-01 19:19:55 -0500

    Add files via upload

---
+++ sesion_2/ejercicio_1/ejercicio_1.js
@@ -0,0 +1,16 @@
+var time = 10;
+var saludo;
+
+if (time < 12) {
+    saludo = "Buenos días";
+}
+
+if (time < 20) {
+    saludo = "Buenas tardes";
+}
+
+if (time >= 20) {
+    saludo = "Buenas noches";
+}
+
+/* /* console.log(saludo); */
