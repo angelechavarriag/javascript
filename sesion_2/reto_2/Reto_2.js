ce65bb81c85b35535d47e833c20ec07b791c2f23
angelechavarriag <82201584+angelechavarriag@users.noreply.github.com>
2021-06-01 19:22:13 -0500

    Add files via upload

---
+++ sesion_2/reto_2/Reto_2.js
@@ -0,0 +1,14 @@
+
+for (var contador = 2; contador <= 100; contador++) {
+    var primo = true;
+  
+    for (var i = 2; i <= contador; i++) {
+      if (contador % i === 0 && i !== contador) {
+        primo = false;
+      }
+    }
+  
+    if(primo) {
+      console.log(contador);
+    }
+  }
